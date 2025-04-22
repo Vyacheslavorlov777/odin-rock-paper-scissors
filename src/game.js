c4096a1ab3476b8087ed0de451f1d835b2a85a27
Vyacheslavorlov777 <Vyacheslavorlov777@gmail.com>
2025-04-22 13:40:02 +0300

    add new logic and design web

--- src/game.js
+++ src/game.js
@@ -1,14 +1,16 @@
-import humanChoice from "./user.js"
+export default (humanChoi, state) => {
 
-export default () => {
-    const state = {
-        humanScore: 0,
-        computerScore: 0,
-    };
-
-    const elemenScoreHuman = document.querySelector('.score-human');
-    const elemenScoreComputer = document.querySelector('.score-computer');
-    const elementResult = document.querySelector('.result-result');
+    const elements = {
+        rock: document.querySelector('.rock-icon'),
+        paper: document.querySelector('.paper-icon'),
+        scisoors: document.querySelector('.scissors-icon'),
+        iconUser: document.querySelector('.icon-user'),
+        iconComputer: document.querySelector('.icon-computer'),
+        elemenScoreHuman: document.querySelector('.score-human'),
+        elemenScoreComputer: document.querySelector('.score-computer'),
+        modal: document.querySelector('.modalBackground'),
+        modalWindow: document.querySelector('.modalWindow'),
+    }
 
     const getComputerChoice = () => {
         const randomElement = Math.floor(Math.random() * 3)
@@ -24,57 +26,86 @@ export default () => {
         }
     }
 
-    const render = () => {
-        if (state.humanScore > state.computerScore) {
-            elementResult.innerHTML = 'You won, congratulation!'
-        } else if (state.humanScore < state.computerScore) {
-            elementResult.innerHTML = 'You lose ('
-        } else {
-            elementResult.innerHTML = 'friendship won (tie)'
+    const render = (humanChoi, computerChoi) => {
+    switch (humanChoi.target.id) {
+        case 'rock':
+            elements.iconUser.innerHTML = `<img src="/src/img/icons8-камень-100.png" alt="" srcset="">`;
+            break;
+        case 'paper':
+            elements.iconUser.innerHTML = `<img src="/src/img/icons8-матовая-бумага-100.png" alt="" srcset="">`
+            break;
+        case 'scissors':
+            elements.iconUser.innerHTML = `<img src="/src/img/icons8-парикмахерские-ножницы-100.png" alt="" srcset="">`
+            break;
+        default:
+            break;
+    }
+
+    switch (computerChoi) {
+        case 'rock':
+            elements.iconComputer.innerHTML = `<img src="/src/img/icons8-камень-100.png" alt="" srcset="">`
+            break;
+        case 'paper':
+            elements.iconComputer.innerHTML = `<img src="/src/img/icons8-матовая-бумага-100.png" alt="" srcset="">`;
+            break;
+        case 'scissors':
+            elements.iconComputer.innerHTML = `<img src="/src/img/icons8-парикмахерские-ножницы-100.png" alt="" srcset="">`;
+            break;
+        default:
+            break;
+    }
+
+        if (state.humanScore === 5) {
+            elements.modal.style.display = 'block';
+            elements.modalWindow.innerHTML = 'You won, congratulation!'
+        } else if (state.computerScore === 5) {
+             elements.modal.style.display = 'block';
+            elements.modalWindow.innerHTML = 'You lose (';
         }
 
-        elemenScoreHuman.innerHTML = state.humanScore;
-        elemenScoreComputer.innerHTML = state.computerScore;
+        elements.elemenScoreHuman.innerHTML = state.humanScore;
+        elements.elemenScoreComputer.innerHTML = state.computerScore;
     }
 
     const game = (valueHuman, valueComp) => {
-        if (valueHuman === valueComp) {
-            return alert('friendship won (tie)');
-        } else if (valueHuman === 'rock' && valueComp === 'paper') {
+        const valueHumanChoi = valueHuman.target.id
+        if (valueHumanChoi === valueComp) {
+            return
+        } else if (valueHumanChoi === 'rock' && valueComp === 'paper') {
             state.computerScore += 1;
-            return alert('you lose');
-        } else if (valueHuman === 'paper' && valueComp === 'rock') {
+            return
+        } else if (valueHumanChoi === 'paper' && valueComp === 'rock') {
             state.humanScore += 1;
-            return alert('you won');
-        } else if (valueHuman === 'scissors' && valueComp === 'rock') {
+            return
+        } else if (valueHumanChoi === 'scissors' && valueComp === 'rock') {
             state.computerScore += 1;
-            return alert('you lose');
-        } else if (valueHuman === 'rock' && valueComp === 'scissors') {
+            return
+        } else if (valueHumanChoi === 'rock' && valueComp === 'scissors') {
             state.humanScore += 1;
-            return alert('you won');
-        }  else if (valueHuman === 'paper' && valueComp === 'scissors') {
+            return
+        }  else if (valueHumanChoi === 'paper' && valueComp === 'scissors') {
             state.computerScore += 1;
-            return alert('you lose');
-        } else if (valueHuman === 'scissors' && valueComp === 'paper') {
+            return
+        } else if (valueHumanChoi === 'scissors' && valueComp === 'paper') {
             state.humanScore += 1;
-            return alert('you won');
+            return
         }
     }
 
     const playRound = (humanChoi, computerChoi) => {
-        const choiHuman = humanChoi.toLowerCase();
-        return game(choiHuman, computerChoi);
+        game(humanChoi, computerChoi);
+        render(humanChoi, computerChoi);
+        return;
     }
 
    
 
-    for (let i = 0; i < 5; i++) {
-        const human = humanChoice()
-        const computer = getComputerChoice()
-        playRound(human, computer);
-    }
-
-    render()
+    // for (let i = 0; i < 5; i++) {
+    //     const human = humanChoice()
+    //     const computer = getComputerChoice()
+    //     playRound(human, computer);
+    // }
+    playRound(humanChoi, getComputerChoice())
 
     return
 }
--- src/js/interactiveGema.js
+++ src/js/interactiveGema.js
@@ -1,3 +1,5 @@
+import play from "../game.js"
+
 const app = () => {
     const state = {
         humanScore: 0,
@@ -8,112 +10,28 @@ const app = () => {
         rock: document.querySelector('.rock-icon'),
         paper: document.querySelector('.paper-icon'),
         scisoors: document.querySelector('.scissors-icon'),
-        iconUser: document.querySelector('.icon-user'),
-        iconComputer: document.querySelector('.icon-computer'),
-    }
+        modal: document.querySelector('.modalBackground'),
+        closed: document.querySelector('.closed'),
+    };
 
     elements.rock.addEventListener('click', (e) => {
         e.preventDefault()
-        elements.iconUser.innerHTML = `<img src="/src/img/icons8-камень-100.png" alt="" srcset="">`
-        elements.iconComputer.innerHTML = computerChoiceHTML(getComputerChoice())
-
+        play(e, state)
     })
     
     elements.paper.addEventListener('click', (e) => {
         e.preventDefault()
-        elements.iconUser.innerHTML = `<img src="/src/img/icons8-матовая-бумага-100.png" alt="" srcset="">`
-        elements.iconComputer.innerHTML = computerChoiceHTML(getComputerChoice())
+        play(e, state)
     })
     
     elements.scisoors.addEventListener('click', (e) => {
         e.preventDefault()
-        elements.iconUser.innerHTML = `<img src="/src/img/icons8-парикмахерские-ножницы-100.png" alt="" srcset="">`
-        elements.iconComputer.innerHTML = computerChoiceHTML(getComputerChoice())
+        play(e, state)
     })
 
-
-    // const elemenScoreHuman = document.querySelector('.score-human');
-    // const elemenScoreComputer = document.querySelector('.score-computer');
-    // const elementResult = document.querySelector('.result-result');
-
-    const getComputerChoice = () => {
-        const randomElement = Math.floor(Math.random() * 3)
-        switch (randomElement) {
-            case 0:
-                return 'rock';
-            case 1:
-                return 'paper';
-            case 2:
-                return 'scissors';
-            default:
-                break;
-        }
-    }
-
-    const computerChoiceHTML = (fn) => {
-        switch (fn) {
-            case 'rock':
-                return `<img src="/src/img/icons8-камень-100.png" alt="" srcset="">`;
-            case 'paper':
-                return `<img src="/src/img/icons8-матовая-бумага-100.png" alt="" srcset="">`;
-            case 'scissors':
-                return `<img src="/src/img/icons8-парикмахерские-ножницы-100.png" alt="" srcset="">`;
-            default:
-                break;
-        }
-    }
-
-    // const render = () => {
-    //     if (state.humanScore > state.computerScore) {
-    //         elementResult.innerHTML = 'You won, congratulation!'
-    //     } else if (state.humanScore < state.computerScore) {
-    //         elementResult.innerHTML = 'You lose ('
-    //     } else {
-    //         elementResult.innerHTML = 'friendship won (tie)'
-    //     }
-
-    //     elemenScoreHuman.innerHTML = state.humanScore;
-    //     elemenScoreComputer.innerHTML = state.computerScore;
-    // }
-
-    // const game = (valueHuman, valueComp) => {
-    //     if (valueHuman === valueComp) {
-    //         return alert('friendship won (tie)');
-    //     } else if (valueHuman === 'rock' && valueComp === 'paper') {
-    //         state.computerScore += 1;
-    //         return alert('you lose');
-    //     } else if (valueHuman === 'paper' && valueComp === 'rock') {
-    //         state.humanScore += 1;
-    //         return alert('you won');
-    //     } else if (valueHuman === 'scissors' && valueComp === 'rock') {
-    //         state.computerScore += 1;
-    //         return alert('you lose');
-    //     } else if (valueHuman === 'rock' && valueComp === 'scissors') {
-    //         state.humanScore += 1;
-    //         return alert('you won');
-    //     }  else if (valueHuman === 'paper' && valueComp === 'scissors') {
-    //         state.computerScore += 1;
-    //         return alert('you lose');
-    //     } else if (valueHuman === 'scissors' && valueComp === 'paper') {
-    //         state.humanScore += 1;
-    //         return alert('you won');
-    //     }
-    // }
-
-    // const playRound = (humanChoi, computerChoi) => {
-    //     const choiHuman = humanChoi.toLowerCase();
-    //     return game(choiHuman, computerChoi);
-    // }
-
-   
-
-    // for (let i = 0; i < 5; i++) {
-    //     const human = humanChoice()
-    //     const computer = getComputerChoice()
-    //     playRound(human, computer);
-    // }
-
-    // render()
+    elements.closed.addEventListener('click', (e) => {
+        elements.modal.style.display = 'none'
+    })
 
     return
 }
