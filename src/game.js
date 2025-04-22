bebd8f2fb8404445235775507bae19fac2b8a9ce
Vyacheslavorlov777 <Vyacheslavorlov777@gmail.com>
2025-04-22 22:04:18 +0300

    add new logic, new interface, new animation

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 export default (humanChoi, state) => {
-
     const elements = {
+        container: document.querySelector('.container'),
         rock: document.querySelector('.rock-icon'),
         paper: document.querySelector('.paper-icon'),
         scisoors: document.querySelector('.scissors-icon'),
@@ -27,45 +27,47 @@ export default (humanChoi, state) => {
     }
 
     const render = (humanChoi, computerChoi) => {
-    switch (humanChoi.target.id) {
-        case 'rock':
-            elements.iconUser.innerHTML = `<img src="/src/img/icons8-камень-100.png" alt="" srcset="">`;
-            break;
-        case 'paper':
-            elements.iconUser.innerHTML = `<img src="/src/img/icons8-матовая-бумага-100.png" alt="" srcset="">`
-            break;
-        case 'scissors':
-            elements.iconUser.innerHTML = `<img src="/src/img/icons8-парикмахерские-ножницы-100.png" alt="" srcset="">`
-            break;
-        default:
-            break;
-    }
+        switch (humanChoi.target.id) {
+            case 'rock':
+                elements.iconUser.innerHTML = `<img id="rock" class="rock-icon icon-main" src="/src/img/rock.PNG" alt="" srcset="">`;
+                break;
+            case 'paper':
+                elements.iconUser.innerHTML = `<img id="paper" class="paper-icon icon-main" src="/src/img/paper.PNG" alt="" srcset="">`;
+                break;
+            case 'scissors':
+                elements.iconUser.innerHTML = `<img id="scissors" class="scissors-icon icon-main" src="/src/img/scissors.PNG" alt="" srcset="">`;
+                break;
+            default:
+                break;
+        }
 
-    switch (computerChoi) {
-        case 'rock':
-            elements.iconComputer.innerHTML = `<img src="/src/img/icons8-камень-100.png" alt="" srcset="">`
-            break;
-        case 'paper':
-            elements.iconComputer.innerHTML = `<img src="/src/img/icons8-матовая-бумага-100.png" alt="" srcset="">`;
-            break;
-        case 'scissors':
-            elements.iconComputer.innerHTML = `<img src="/src/img/icons8-парикмахерские-ножницы-100.png" alt="" srcset="">`;
-            break;
-        default:
-            break;
+        switch (computerChoi) {
+            case 'rock':
+                elements.iconComputer.innerHTML = `<img id="rock" class="rock-icon icon-main" src="/src/img/comp-rock.PNG" alt="" srcset="">`;
+                break;
+            case 'paper':
+                elements.iconComputer.innerHTML = `<img id="paper" class="paper-icon icon-main" src="/src/img/comp-paper.PNG" alt="" srcset="">`;
+                break;
+            case 'scissors':
+                elements.iconComputer.innerHTML = `<img id="scissors" class="scissors-icon icon-main" src="/src/img/comp-scissors.PNG" alt="" srcset="">`;
+                break;
+            default:
+                break;
     }
 
         if (state.humanScore === 5) {
             elements.modal.style.display = 'block';
-            elements.modalWindow.innerHTML = 'You won, congratulation!'
+            elements.modalWindow.innerHTML = 'You won, congratulation!';
+            elements.container.style = 'background-image: url(/src/img/the-end.JPG)';
         } else if (state.computerScore === 5) {
-             elements.modal.style.display = 'block';
+            elements.modal.style.display = 'block';
             elements.modalWindow.innerHTML = 'You lose (';
+            elements.container.style = 'background-image: url(/src/img/the-end.JPG)';
         }
 
         elements.elemenScoreHuman.innerHTML = state.humanScore;
         elements.elemenScoreComputer.innerHTML = state.computerScore;
-    }
+    };
 
     const game = (valueHuman, valueComp) => {
         const valueHumanChoi = valueHuman.target.id
@@ -90,15 +92,14 @@ export default (humanChoi, state) => {
             state.humanScore += 1;
             return
         }
-    }
+    };
 
     const playRound = (humanChoi, computerChoi) => {
         game(humanChoi, computerChoi);
         render(humanChoi, computerChoi);
         return;
-    }
+    };
 
-    playRound(humanChoi, getComputerChoice())
-
-    return
-}
+    playRound(humanChoi, getComputerChoice());
+    return;
+};
--- src/user.js
+++
@@ -1,5 +0,0 @@
-export default () => {
-    let sign = prompt("What do you choose: rock, paper or scissors?");
-    return sign
-}
-
